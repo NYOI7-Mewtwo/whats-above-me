9d2180a9229be2e37b27553ded1a1d5bdc929a30
James Huang <jhuang4647@gmail.com>
2023-11-08 14:07:28 -0500

    added coordinates context for latitude and longitude

--- client/App.tsx
+++ client/App.tsx
@@ -1,13 +1,16 @@
-import TopContainer from "./containers/TopContainer";
-import BottomContainer from "./containers/BottomContainer";
+import TopContainer from './containers/TopContainer';
+import BottomContainer from './containers/BottomContainer';
+import { CoordinatesContextProvider } from './context/CoordinatesContext';
 
 const App = () => {
-    return (
-        <>
-            <TopContainer />
-            <BottomContainer />
-        </>
-    );
+  return (
+    <>
+      <CoordinatesContextProvider>
+        <TopContainer />
+        <BottomContainer />
+      </CoordinatesContextProvider>
+    </>
+  );
 };
 
 export default App;
